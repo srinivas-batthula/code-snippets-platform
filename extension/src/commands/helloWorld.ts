8fb477082d25e6f524e47762acccb0e212e95fae
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2025-09-07 19:55:26 +0530

    added starter VSCode Extension setup (#2)
    * added next-auth apis
    
    * vscode-extension setup

---
+++ extension/src/commands/helloWorld.ts
@@ -0,0 +1,29 @@
+// src/commands/helloWorld.ts
+import * as vscode from 'vscode';
+import { getHelloPanelHtml } from '../panels/helloPanel';
+import { log } from '../utils/logger';
+
+export function registerHelloWorld(context: vscode.ExtensionContext) {
+    const command = vscode.commands.registerCommand(
+        'codecapsule.helloWorld',
+        () => {
+            const panel = vscode.window.createWebviewPanel(
+                'helloWorld',
+                'Hello Panel',
+                vscode.ViewColumn.One,
+                { enableScripts: true }
+            );
+            panel.webview.html = getHelloPanelHtml();
+            log('Opened Hello Panel : CodeCapsule');
+
+            panel.webview.onDidReceiveMessage(
+                message => {
+                    vscode.commands.executeCommand(`codecapsule.${message.command}`);
+                },
+                undefined,
+                context.subscriptions
+            );
+        }
+    );
+    context.subscriptions.push(command);
+}
